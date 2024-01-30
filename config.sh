a663ecde4e853726474175f775a16d739cca1f14
Mohammad Ghoreishi <mohammadghoreishi@mohammads-air.home>
2024-01-29 20:33:26 -0500

    added cache

--- config.sh
+++ config.sh
@@ -6,14 +6,13 @@ then
   ls -la config/
   cat config/service1/service1.json
   cat config/service2/service2.json
+else
+  sleep 5
+  mkdir config 
+  mkdir config/service1 config/service2
+  touch config/service1/service1.json
+  touch config/service2/service2.json
 fi
-
-mkdir config 
-mkdir config/service1 config/service2
-touch config/service1/service1.json
-touch config/service2/service2.json
-
-sleep 5
 
 echo "{'service': 'one'}" > config/service1/service1.json
 echo "{'service': 2}" > config/service2/service2.json
